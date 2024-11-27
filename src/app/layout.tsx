e0f894948a845cf200d0c0d48ec83613f9842c5d
sanskarbasnet <53053565+sanskarbasnet@users.noreply.github.com>
2024-11-27 00:42:05 +0000

    metadata changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: `${DATA.name}`,
     description: DATA.description,
-    url: "/thumbnail.png",
+    url: "/public/thumbnail.png",
     siteName: `${DATA.name}`,
     locale: "en_US",
     type: "website",
